d79227a9f88b4aefdd7ef60bdf674e98fe2a6d69
rochmadnf <rochmadnf@gmail.com>
2021-03-03 00:43:42 +0800

    V1.1.0 {User} changes log:  membuat controller User  membuat function/method edit untuk mengelola edit pada user  mengaktifkan namespace pada RouteServiceProvider.php agar akses controller pada web.php seperti pada laravel 7.x  membuat halaman untuk melakukan edit pada user  menambah sintaks pada input.blade.php agar dapat menampung data untuk melakukan pengubahan pada data tersebut  membuat route untuk mengakses halaman user edit

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -2,7 +2,11 @@
     <label for="{{ $name }}" class="col-md-4 col-form-label text-md-right">{{ $label }}</label>
 
     <div class="col-md-6">
-        <input id="{{ $name }}" type="{{$type ?? 'text'}}" class="form-control @error($name) is-invalid @enderror" name="{{ $name }}" value="{{ old($name) }}" {{ $otherAttr ?? '' }}>
+        <input id="{{ $name }}" type="{{$type ?? 'text'}}" class="form-control @error($name) is-invalid @enderror"
+          name="{{ $name }}"
+          @isset(($object->{$name})) value="{{ old($name) ? old($name) : $object->{$name} }}"
+          @else value="{{ old($name) }}" @endisset
+          {{ $otherAttr ?? '' }}>
 
         @if ($errors->has($name))
         <span class="invalid-feedback" role="alert">
---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">
+            Update Profile <strong>{{ $user->username }}</strong>
+          </div>
+          <div class="card-body">
+            <form method="POST" action="/user/update">
+              @csrf
+
+              <x-input label="Username" name="username" otherAttr="autofocus" :object="$user"/>
+              <x-input label="Email" name="email" type="email" :object="$user"/>
+              <x-input label="Biodata" name="bio" :object="$user"/>
+
+              <p>
+                Todo: Avatar
+              </p>
+
+              <x-submitbtn name="Update Profile" />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+@endsection
